c6ed81126706f64cf78524ee079f9e35ed717fa9
nephylum <25045705+nephylum@users.noreply.github.com>
2020-07-08 13:53:53 -0700

    working

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,9 +1,25 @@
-def word_count(s):
-    # Implement me.
 
+def word_count(s):
+    cache = {}
+    s = s.lower()
+    punctuation = '''!()-[]{};:"\|,+=<>./?@#$%^&*_~'''
+    news = ""
+    for char in s:
+        if char not in punctuation:
+            news += char
+
+    print(news)
+    listostrings=news.split()
+    for item in listostrings:
+        if item not in cache:
+            cache[item] = 1
+        else:
+            cache[item] += 1
+    return cache
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
     print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    
