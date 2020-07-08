c9705b23d2589e7c595f8cb6859fd3f88b317e05
nephylum <25045705+nephylum@users.noreply.github.com>
2020-07-08 14:44:41 -0700

    WIP

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -3,6 +3,14 @@ import random
 # Read in all the words in one go
 with open("input.txt") as f:
     words = f.read()
+listowords = words.split()
+#print(listowords)
+cache = {}
+for x in range(len(listowords)):
+    if x < len(listowords) - 1:
+        cache[listowords[x]] += listowords[x + 1]
+
+print(cache)
 
 # TODO: analyze which words can follow other words
 
