82fd2be5d663f6d5a7f7ddcec2d3b695ea3ea341
nephylum <25045705+nephylum@users.noreply.github.com>
2020-07-09 23:22:20 -0700

    working

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -20,6 +20,42 @@ for x in range(len(listowords)):
     elif listowords[x] in cache:
         if x < len(listowords) - 1:
             cache[listowords[x]].append(listowords[x + 1])
-print(len(cache['the']))
 
-# TODO: construct 5 random sentences
+#create function to create random sentences
+def make_sentence(cache):
+    # define the stop punctuation to finish a sentence
+    stop = '.!?'
+    new_sentence = ""
+    # pick a random start word
+    choices = list(cache.keys())
+    choice = random.choice(choices)
+    # print('choice:' , choice, '\nvalues:', cache[choice])
+    #if it's a stop word, end sentence
+    if choice[-1] in stop:
+        new_sentence = choice
+
+        return new_sentence
+    # otherwise, pick random next word (from key's values)
+    else:
+        new_sentence = choice
+        while len(new_sentence) > 0:
+            choice= random.choice(cache[choice])
+            if choice[-1] == '"':
+                if choice[-2] in stop:
+                    new_sentence += (" " + choice)
+                    return new_sentence
+            elif choice[-1] in stop:
+                new_sentence += (' ' + choice)
+                return new_sentence
+            else:
+                new_sentence += (" " + choice)
+
+    return new_senctence
+
+# construct 5 random sentences
+
+print(make_sentence(cache), "\n")
+print(make_sentence(cache), "\n")
+print(make_sentence(cache), "\n")
+print(make_sentence(cache), "\n")
+print(make_sentence(cache))
