3895b16fecabeaef658a154010e5af62f695107c
nephylum <25045705+nephylum@users.noreply.github.com>
2020-07-09 22:49:18 -0700

    WIP

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -5,13 +5,21 @@ with open("input.txt") as f:
     words = f.read()
 listowords = words.split()
 #print(listowords)
-cache = {}
-for x in range(len(listowords)):
-    if x < len(listowords) - 1:
-        cache[listowords[x]] += listowords[x + 1]
-
-print(cache)
 
 # TODO: analyze which words can follow other words
+
+cache = {}
+for x in range(len(listowords)):
+# if word doesn't exist in cache, create a new word and assign the next word
+# as the key value
+    if listowords[x] not in cache:
+        if x < len(listowords) - 1:
+            cache[listowords[x]] = [listowords[x + 1]]
+        else:
+            cache[listowords[x]] = ""
+    elif listowords[x] in cache:
+        if x < len(listowords) - 1:
+            cache[listowords[x]].append(listowords[x + 1])
+print(len(cache['the']))
 
 # TODO: construct 5 random sentences
